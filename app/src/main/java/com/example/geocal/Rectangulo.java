91ab758c14299eba936a49396a77744d2310a7d4
Cristofer <cristofer.sereno@xemantics.com>
2019-08-29 13:10:50 -0400

    Cambios codigo

--- app/src/main/java/com/example/geocal/Rectangulo.java
+++ app/src/main/java/com/example/geocal/Rectangulo.java
@@ -5,17 +5,53 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class Rectangulo extends AppCompatActivity {
 
     private Button btnVolver;
+    private Button btnCalcular;
+    private EditText etArea,etPerimetro,etDiagonal;
+    private TextView textViewResultado;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_rectangulo);
 
+
+
+        //Botones
         btnVolver  = (Button)findViewById(R.id.btnVolver);
+        btnVolver  = (Button)findViewById(R.id.btnVolver);
+        btnCalcular  = (Button)findViewById(R.id.btnCalcular);
+        //Variables de numeros para el calculo
+
+        etArea = (EditText)findViewById(R.id.etArea);
+        etDiagonal = (EditText)findViewById(R.id.etDiagonal);
+        etPerimetro = (EditText)findViewById(R.id.etPerimetro);
+
+        //Variable resultado
+        textViewResultado = (TextView)findViewById(R.id.textViewResultado);
+
+
+        btnCalcular.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                int area = Integer.parseInt(etArea.getText().toString());
+                int diagonal = Integer.parseInt(etDiagonal.getText().toString());
+                int perimetro = Integer.parseInt(etPerimetro.getText().toString());
+                int suma = (area+diagonal+perimetro);
+                String resu = String.valueOf(suma);
+                textViewResultado.setText(resu);
+
+
+            }
+        });
 
         btnVolver.setOnClickListener(new View.OnClickListener() {
             @Override
--- app/src/main/java/com/example/geocal/Triangulos.java
+++ app/src/main/java/com/example/geocal/Triangulos.java
@@ -5,12 +5,17 @@ import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 public class Triangulos extends AppCompatActivity {
 
     private Button btnVolver;
     private Button btnCalcular;
+    private EditText etArea,etPerimetro,etDiagonal;
+    private TextView textViewResultado;
+
 
 
     @Override
@@ -18,14 +23,32 @@ public class Triangulos extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_triangulos);
 
+        //Botones
+        btnVolver  = (Button)findViewById(R.id.btnVolver);
         btnVolver  = (Button)findViewById(R.id.btnVolver);
         btnCalcular  = (Button)findViewById(R.id.btnCalcular);
+        //Variables de numeros para el calculo
+
+        etArea = (EditText)findViewById(R.id.etArea);
+        etDiagonal = (EditText)findViewById(R.id.etDiagonal);
+        etPerimetro = (EditText)findViewById(R.id.etPerimetro);
+
+        //Variable resultado
+        textViewResultado = (TextView)findViewById(R.id.textViewResultado);
+
 
         btnCalcular.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                Toast.makeText(getApplicationContext(),"Presiono boton calcular",Toast.LENGTH_LONG).show();
+                int area = Integer.parseInt(etArea.getText().toString());
+                int diagonal = Integer.parseInt(etDiagonal.getText().toString());
+                int perimetro = Integer.parseInt(etPerimetro.getText().toString());
+                int suma = (area+diagonal+perimetro);
+                String resu = String.valueOf(suma);
+                textViewResultado.setText(resu);
+
+
             }
         });
 
